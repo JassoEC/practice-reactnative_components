2834dab8baa6007e4329fdbea592fca9ccfb9be9
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-05-18 22:56:12 -0500

    se creo  la animacion de fadein y de rebote

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,15 +1,59 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, Button, Easing, StyleSheet, View} from 'react-native';
 
 export const Animation101Screen = () => {
+  const opacity = useRef(new Animated.Value(0)).current;
+  const top = useRef(new Animated.Value(-100)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => console.log('fin animacion'));
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.purpleBox} />
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          ...styles.purpleBox,
+          marginBottom: 20,
+          opacity,
+          transform: [
+            {
+              translateY: top,
+            },
+          ],
+        }}
+      />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="FadeOut" onPress={fadeOut} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   purpleBox: {
     backgroundColor: '#5856D6',
     width: 150,
