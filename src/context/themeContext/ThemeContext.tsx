aa3955cfc57e1f9515f0e08f4869c6c124ff9abb
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-05-23 23:02:06 -0500

    se configuro el listener para mantener  el tema de acuerdo con las configuraciones del dispositivo

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, useReducer} from 'react';
-import {ligthTheme, themeReducer, ThemeState} from './themeReducer';
+import React, {createContext, useEffect, useReducer} from 'react';
+import {Appearance, AppState, useColorScheme} from 'react-native';
+import {darkTheme, ligthTheme, themeReducer, ThemeState} from './themeReducer';
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -9,7 +10,12 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({children}: any) => {
-  const [theme, dispatch] = useReducer(themeReducer, ligthTheme); //TODO: leet tmea d telefno
+  const colorScheme = useColorScheme();
+  const [theme, dispatch] = useReducer(
+    themeReducer,
+    colorScheme === 'dark' ? darkTheme : ligthTheme,
+  );
+
   const setDarkTheme = () => {
     console.log('set Dark Theme');
     dispatch({type: 'set_dark_theme'});
@@ -18,6 +24,16 @@ export const ThemeProvider = ({children}: any) => {
     console.log('set Light theme');
     dispatch({type: 'set_light_theme'});
   };
+
+  useEffect(() => {
+    AppState.addEventListener('change', status => {
+      if (status === 'active') {
+        Appearance.getColorScheme() === 'light'
+          ? setLightTheme()
+          : setDarkTheme();
+      }
+    });
+  }, []);
 
   return (
     <ThemeContext.Provider value={{setDarkTheme, setLightTheme, theme}}>
